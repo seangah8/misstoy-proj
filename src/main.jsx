14c4340238a9c88289ce0e6be2f65f442eb6a0d9
seangah8 <seangah8@gmail.com>
2025-01-02 07:07:46 +0200

    imlement scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/scss/view/style.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
